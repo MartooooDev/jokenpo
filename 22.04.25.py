f2a21dbbefd085645a66b4bf14e79ef4eacee074
Martin <martin@cosmoswide.com.br>
2025-04-23 11:01:21 -0300

    Adiciona modo Jogador VS Jogador

--- 22.04.25.py
+++ 22.04.25.py
@@ -14,7 +14,7 @@ print('Bem-vindo ao Jokenpo!');
 exit = 0;
 
 #Apresentação das opções
-while opcao != 1:
+while exit != 1:
     print('Selecione uma opção para jogar: ');
     print('1- Jogador VS Jogador');
     print('2- Jogador VS COM');
@@ -26,7 +26,74 @@ while opcao != 1:
     print('----------------------------------');
 
     #Jogador VS Jogador
+    if opcao == 1:
+        print('Você selecionou Jogador VS Jogador');
+        print('-------------------------------------\n');
+        print('Caso não estejam cientes das regras, aqui está um resumo simples: ')
+        print('Pedra ganha de Tesoura');
+        print('Tesoura ganha de Papel');
+        print('Papel ganha de Pedra');
+        print('-------------------------------------\n');
 
+        print('Vamos lá!');
+        print('Quem está jogando?');
+
+        nome_j1 = input('Digite o nome do jogador 1: ');
+        nome_j2 = input('Digite o nome do jogador 2: ');
+
+        pontuacao_j1 = 0;
+        pontuacao_j2 = 0;
+
+        print(f'{nome_j1}, {nome_j2}, prazer em tê-los aqui!');
+        print('Vamos começar o jogo!');
+        print('-------------------------------------\n');
+
+        print(f'{nome_j1}, escolha uma das opções:')
+        print('1- Pedra');
+        print('2- Papel');
+        print('3- Tesoura\n');
+
+        escolha_j1 = int(input(''));
+
+        print(f'{nome_j2}, escolha uma das opções:')
+        print('1- Pedra');
+        print('2- Papel');
+        print('3- Tesoura\n');
+
+        escolha_j2 = int(input(''));
+
+        print('-------------------------------------\n');
+        print( '|       J1       |       J2       \n')
+        print(f'|       {escolha_j1}        |       {escolha_j2}       \n')
+        print('-------------------------------------\n');
+
+        if escolha_j1 == escolha_j2:
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            print('Empate!');
+        elif (escolha_j1 == 1 and escolha_j2 == 2) or (escolha_j1 == 2 and escolha_j2 == 3) or (escolha_j1 == 3 and escolha_j2 == 1):
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            input(f'{nome_j2} ganhou!');
+            pontuacao_j2 += 1;
+        else:
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            sleep(1);
+            print('.')
+            input(f'{nome_j1} ganhou!');
+            pontuacao_j1 += 1;
+            
     #Jogador VS COM
 
     #COM VS COM
@@ -59,9 +126,9 @@ while opcao != 1:
         elif (escolha_cpu1 == 1 and escolha_cpu2 == 2) or \
             (escolha_cpu1 == 2 and escolha_cpu2 == 3) or \
             (escolha_cpu1 == 3 and escolha_cpu2 == 1):
-            print("cpu 1 venceu!")
+            input("cpu 1 venceu!")
         else:
-            print("cpu 2 venceu!")
+            input("cpu 2 venceu!")
 
     #Sair
     if opcao == 4:
