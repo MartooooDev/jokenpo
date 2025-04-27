f34b199d58e3bb62983f443912926101b080ade0
Martin <martin.romao@hotmail.com>
2025-04-27 00:41:14 -0300

    Padronização geral e correção de erros

--- 22.04.25.py
+++ 22.04.25.py
@@ -1,227 +1,284 @@
 # --------------------------------------------------
 # Trabalho da disciplina de Raciocínio Algorítmico
-# Pedra, papel, tesoura;
+# Pedra, papel, tesoura
 #
 # Data: 16/04/2025
 # --------------------------------------------------
+
 import random
 import os
 from time import sleep
 
 #Início do jogo
-print('Bem-vindo ao Jokenpo!');
-exit = 0;
+print('Bem-vindo ao Jokenpo!')
+exit = 0
 sleep(1)
 
 #Apresentação das opções
 while exit != 1:
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear') #Em uma pesquisa, econtrei que no Linux o comando é diferente (Martin usando Ubuntu)
 
-    continuar = 0;
-    pontuacao_j1 = 0;
-    pontuacao_j2 = 0;
+    continuar = 0
+    pontuacao_j1 = 0
+    pontuacao_j2 = 0
     
-    print('Selecione uma opção para jogar: ');
-    print('1- Jogador VS Jogador');
-    print('2- Jogador VS Computador');
-    print('3- Computador VS Computador');
-    print('4- Sair');
+    print('Selecione uma opção para jogar: ')
+    print('1- Jogador VS Jogador')
+    print('2- Jogador VS Computador')
+    print('3- Computador VS Computador')
+    print('4- Sair')
 
-    print('----------------------------------');
-    opcao = int(input('Sua escolha: '));
-    print('----------------------------------');
+    print('+' + '-' * 30 + '+')
+    opcao = 0
+    while opcao < 1 or opcao > 4:
+        opcao = int(input('Sua escolha: '))
+        if opcao < 1 or opcao > 4:
+            print("*Opção inválida. Escolha entre 1 e 4.*")
+    print('+' + '-' * 30 + '+')
     
     #Sair
     if opcao == 4:
-        print('Saindo do jogo...');
-        sleep(1);
-        exit = 1;
-        break;
+        print('Saindo do jogo...')
+        sleep(1)
+        print('Programa feito por Elgson Nascimento, Martin Romão e Maria Pietra.')
+        print('Obligado por jogar com a gente!')
+        sleep(1)
+        exit = 1
+        break
 
-    if opcao == 1 or opcao == 2 :
+    if opcao == 1 :
         nome_j1 = input("Digite o nome do jogador 1:\n")
         nome_j2 = input("Digite o nome do jogador 2:\n")
     elif opcao == 2:
         nome_j1 = input("Digite o nome do jogador 1:\n")
-        nome_j2 = 'COM';
+        nome_j2 = 'COM'
     elif opcao == 3 :
-        nome_j1 = 'COM';
-        nome_j2 = 'COM';
+        nome_j1 = 'COM1'
+        nome_j2 = 'COM2'
 
     while continuar != 2:
         #Jogador VS Jogador
         if opcao == 1:
-            print('Você selecionou Jogador VS Jogador');
-            print('-------------------------------------\n');
+            print('Você selecionou Jogador VS Jogador')
+            print('+' + '-' * 30 + '+')
             print('Caso não estejam cientes das regras, aqui está um resumo simples: ')
-            print('Pedra ganha de Tesoura');
-            print('Tesoura ganha de Papel');
-            print('Papel ganha de Pedra');
-            print('-------------------------------------\n');
+            print('Pedra ganha de Tesoura')
+            print('Tesoura ganha de Papel')
+            print('Papel ganha de Pedra')
+            print('+' + '-' * 30 + '+')
 
-            print('Vamos lá!');
-            print(f'{nome_j1}, {nome_j2}, prazer em tê-los aqui!');
-            print('Vamos começar o jogo!');
-            print('-------------------------------------\n');
+            print(f'{nome_j1}, {nome_j2}, prazer em tê-los aqui!')
+            print('Vamos começar o jogo!')
+            print('+' + '-' * 30 + '+')
 
-            print(f'{nome_j1}, escolha uma das opções:')
-            print('1- Pedra');
-            print('2- Papel');
-            print('3- Tesoura\n');
+            print('1- Pedra')
+            print('2- Papel')
+            print('3- Tesoura\n')
 
-            escolha_j1 = int(input(''));
+            escolha_j1 = 0
+            while escolha_j1 < 1 or escolha_j1 > 3:
+                escolha_j1 = int(input(f'{nome_j1}, Escolha uma das opções (1, 2 ou 3): '))
+                if escolha_j1 < 1 or escolha_j1 > 3:
+                    print("*Número inválido. Escolha 1, 2 ou 3.*")
 
-            print(f'{nome_j2}, escolha uma das opções:')
-            print('1- Pedra');
-            print('2- Papel');
-            print('3- Tesoura\n');
+            if escolha_j1 == 1:
+                escolha_j1_convertido = "Pedra"
+            elif escolha_j1 == 2:
+                escolha_j1_convertido = "Papel"
+            else:
+                escolha_j1_convertido = "Tesoura"
 
-            escolha_j2 = int(input(''));
+            print('+' + '-' * 30 + '+')
 
-            print('-------------------------------------\n');
-            print( '|       J1       |       J2       \n')
-            print(f'|       {escolha_j1}        X       {escolha_j2}       \n')
-            print('-------------------------------------\n');
+            print('\n\n1- Pedra')
+            print('2- Papel')
+            print('3- Tesoura\n')
+
+            escolha_j2 = 0
+            while escolha_j2 < 1 or escolha_j2 > 3:
+                escolha_j2 = int(input(f'{nome_j2}, Escolha uma das opções (1, 2 ou 3):'))
+                if escolha_j2 < 1 or escolha_j2 > 3:
+                    print("*Número inválido. Escolha 1, 2 ou 3.*")
+
+            if escolha_j2 == 1:
+                escolha_j2_convertido = "Pedra"
+            elif escolha_j2 == 2:
+                escolha_j2_convertido = "Papel"
+            else:
+                escolha_j2_convertido = "Tesoura"
+
+            sleep(2)
+            print('+' + '-' * 30 + '+')
+            print(f'| {"ESCOLHAS DA RODADA":^28} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {nome_j1:^10} | {nome_j2:^10} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {escolha_j1_convertido:^10} | {escolha_j2_convertido:^10} |')
+            print('+' + '-' * 30 + '+')
+            sleep(2)
 
             if escolha_j1 == escolha_j2:
-                sleep(1), print('.'), sleep(1), print('.'), sleep(1), print('.')
-                print('Empate!');
+                sleep(1), print('.'), sleep(1), print('.')
+                print('Empate!')
+                sleep(2)
             elif (escolha_j1 == 1 and escolha_j2 == 2) or (escolha_j1 == 2 and escolha_j2 == 3) or (escolha_j1 == 3 and escolha_j2 == 1):
-                sleep(1), print('.'), sleep(1), print('.'), sleep(1), print('.')
-                input(f'{nome_j2} ganhou!');
-                pontuacao_j2 += 1;
+                sleep(1), print('.'), sleep(1), print('.')
+                print(f'{nome_j2} ganhou!')
+                sleep(2)
+                pontuacao_j2 += 1
             else:
-                sleep(1), print('.'), sleep(1), print('.'), sleep(1), print('.')
-                input(f'{nome_j1} ganhou!');
-                pontuacao_j1 += 1;
+                sleep(1), print('.'), sleep(1), print('.')
+                print(f'{nome_j1} ganhou!')
+                sleep(2)
+                pontuacao_j1 += 1
                 
-            continuar = int(input("Deseja continuar? \n1- Sim\n2- Não"))
+            continuar = 0
+            while continuar < 1 or continuar > 2:
+                continuar = int(input("Deseja continuar? \n1- Sim\n2- Não\n"))
+                if continuar < 1 or continuar > 2:
+                    print("*Opção inválida. Escolha 1 ou 2.*")
 
         #Jogador VS COMPUTADOR
         if opcao == 2:
-            print("Modo: Jogador vs Computador Selecionado!\n")
+            print("Você selecionou o modo Jogador vs Computador!\n")
 
-            # Loop das partidas dentro do modo JvC
-            # while True:
-            print(f"\n--- Placar: Jogador {pontuacao_j1} x {pontuacao_j2} Computador ---")
-            print("Opções: 1: Pedra | 2: Tesoura | 3: Papel")
+            print(f'{nome_j1}, {nome_j2}, prazer em tê-los aqui!')
+            print('Vamos começar o jogo!')
+            print('+' + '-' * 30 + '+')
 
-            # Obter jogada do jogador (número) com validação
-            escolha_jogador_num = 0
-            # Validação usa a lista [1, 2, 3] diretamente, não 'opcoes_jogo'
-            while escolha_jogador_num not in [1, 2, 3]:
-                try:
-                    entrada_jogador = input("Sua escolha (1, 2 ou 3): ").strip()
-                    if entrada_jogador: # Verifica se não está vazio
-                        escolha_jogador_num = int(entrada_jogador)
-                        if escolha_jogador_num not in [1, 2, 3]:
-                            print("   *Número inválido. Escolha 1, 2 ou 3.*")
-                    else:
-                        print("   *Entrada vazia. Escolha 1, 2 ou 3.*")
-                except ValueError:
-                    print("   *Entrada inválida. Por favor, digite um NÚMERO (1, 2 ou 3).*")
+            print('1- Pedra')
+            print('2- Papel')
+            print('3- Tesoura\n')
 
+            escolha_jogador = 0
+            while escolha_jogador < 1 or escolha_jogador > 3:
+                escolha_jogador = int(input("Escolha uma das opções (1, 2 ou 3): "))
+                if escolha_jogador < 1 or escolha_jogador > 3:
+                    print("*Número inválido. Escolha 1, 2 ou 3.*")
 
             # Gerar jogada do computador (número)
-            escolha_computador_num = random.randint(1, 3) # <<< Precisa do 'import random'
+            escolha_computador = random.randint(1, 3)
 
             # Converter número da escolha do jogador para nome
-            if escolha_jogador_num == 1:
-                nome_jogador = "Pedra"
-            elif escolha_jogador_num == 2:
-                nome_jogador = "Tesoura"
-            else: # Se não for 1 ou 2, só pode ser 3
-                nome_jogador = "Papel"
+            if escolha_jogador == 1:
+                escolha_jogador_convertido = "Pedra"
+            elif escolha_jogador == 2:
+                escolha_jogador_convertido = "Papel"
+            else:
+                escolha_jogador_convertido = "Tesoura"
 
             # Converter número da escolha do computador para nome
-            if escolha_computador_num == 1:
-                nome_computador = "Pedra"
-            elif escolha_computador_num == 2:
-                nome_computador = "Tesoura"
-            else: # Se não for 1 ou 2, só pode ser 3
-                nome_computador = "Papel"
+            if escolha_computador == 1:
+                escolha_computador_convertido = "Pedra"
+            elif escolha_computador == 2:
+                escolha_computador_convertido = "Papel"
+            else:
+                escolha_computador_convertido = "Tesoura"
 
             # Mostrar as escolhas com nomes
-            print(f"\nVocê escolheu: {nome_jogador}")
-            print(f"Computador escolheu: {nome_computador}")
+            sleep(2)
+            print('+' + '-' * 30 + '+')
+            print(f'| {"ESCOLHAS DA RODADA":^28} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {nome_j1:^10} | {nome_j2:^10} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {escolha_jogador_convertido:^10} | {escolha_computador_convertido:^10} |')
+            print('+' + '-' * 30 + '+')
+            sleep(2)
 
             # Lógica do Jokenpô usando os números (1:Pedra, 2:Tesoura, 3:Papel)
-            if escolha_jogador_num == escolha_computador_num:
-                print("Resultado: Empate!")
-            elif (escolha_jogador_num == 1 and escolha_computador_num == 2) or \
-                (escolha_jogador_num == 2 and escolha_computador_num == 3) or \
-                (escolha_jogador_num == 3 and escolha_computador_num == 1):
-                print("Resultado: Você venceu esta rodada!")
+            if escolha_jogador == escolha_computador:
+                sleep(1), print('.'), sleep(1), print('.')
+                print('Empate!')
+                sleep(2)
+            elif (escolha_jogador == 1 and escolha_computador == 2) or \
+                (escolha_jogador == 2 and escolha_computador == 3) or \
+                (escolha_jogador == 3 and escolha_computador == 1):
+                sleep(1), print('.'), sleep(1), print('.')
+                print("Você venceu esta rodada!")
+                sleep(2)
                 pontuacao_j1 += 1
             else:
-                print("Resultado: Computador venceu esta rodada!")
+                sleep(1), print('.'), sleep(1), print('.')
+                print("O computador venceu esta rodada!")
+                sleep(2)
                 pontuacao_j2 += 1
 
-            print("-" * 25) # Separador
+            print("-" * 25)
 
-            # # Perguntar se quer continuar neste modo
-            # continuar_neste_modo = ""
-            # while continuar_neste_modo not in ['s', 'n']:
-            #     continuar_neste_modo = input("Jogar outra partida neste modo? (s/n): ").lower().strip()
-            #     if continuar_neste_modo not in ['s', 'n']:
-            #         print("   *Digite 's' para sim ou 'n' para não.*")
-
-            # # Se não quer continuar, volta ao menu principal
-            # if continuar_neste_modo == 'n':
-            #     print("\nVoltando ao menu principal...")
-            #     sleep(1.5) # <<< Precisa do 'import time'
-            #     break # Sai do loop de partidas (JvC)
-
-            # # Se digitou 's', o loop continua para a próxima rodada
-            # os.system('cls' if os.name == 'nt' else 'clear') # <<< Precisa do 'import os'
-
-            continuar = int(input("Deseja continuar? \n1- Sim\n2- Não"))
+            continuar = 0
+            while continuar < 1 or continuar > 2:
+                continuar = int(input("Deseja continuar? \n1- Sim\n2- Não\n"))
+                if continuar < 1 or continuar > 2:
+                    print("*Opção inválida. Escolha 1 ou 2.*")
                 
         #COM VS COM
         if opcao == 3:
-            print("cpu vs cpu")
+            print("Você selecionou o modo Computador vs Computador!\n")
+
+            print(f'{nome_j1}, {nome_j2}, prazer em tê-los aqui!')
+            print('Vamos começar o jogo!')
+            print('+' + '-' * 30 + '+')
             escolha_cpu1 = random.randint(1, 3)  
             escolha_cpu2 = random.randint(1, 3) 
 
             if escolha_cpu1 == 1:
-                nome_cpu1 = "Pedra"
+                escolha_cpu1_convertido = "Pedra"
             elif escolha_cpu1 == 2:
-                nome_cpu1 = "Tesoura"
+                escolha_cpu1_convertido = "Papel"
             else:
-                nome_cpu1 = "Papel"
+                escolha_cpu1_convertido = "Tesoura"
 
             if escolha_cpu2 == 1:
-                nome_cpu2 = "Pedra"
+                escolha_cpu2_convertido = "Pedra"
             elif escolha_cpu2 == 2:
-                nome_cpu2 = "Tesoura"
+                escolha_cpu2_convertido = "Papel"
             else:
-                nome_cpu2 = "Papel"
+                escolha_cpu2_convertido = "Tesoura"
 
-            print(f"cpu 1 escolheu: {nome_cpu1}")
-            print(f"cpu 2 escolheu: {nome_cpu2}")
+            sleep(2)
+            print('+' + '-' * 30 + '+')
+            print(f'| {"ESCOLHAS DA RODADA":^28} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {nome_j1:^10} | {nome_j2:^10} |')
+            print('+' + '-' * 30 + '+')
+            print(f'| {escolha_cpu1_convertido:^10} | {escolha_cpu2_convertido:^10} |')
+            print('+' + '-' * 30 + '+')
+            sleep(2)
 
             # Lógica do Jokenpô
             if escolha_cpu1 == escolha_cpu2:
+                sleep(1), print('.'), sleep(1), print('.')
                 print("Empate!")
+                sleep(2)
             elif (escolha_cpu1 == 1 and escolha_cpu2 == 2) or \
                 (escolha_cpu1 == 2 and escolha_cpu2 == 3) or \
                 (escolha_cpu1 == 3 and escolha_cpu2 == 1):
-                input("cpu 1 venceu!")
-                pontuacao_j1 += 1;
+                sleep(1), print('.'), sleep(1), print('.')
+                print("cpu 2 venceu!")
+                sleep(2)
+                pontuacao_j2 += 1
             else:
-                input("cpu 2 venceu!")
-                pontuacao_j2 += 1;
+                sleep(1), print('.'), sleep(1), print('.')
+                print("cpu 1 venceu!")
+                sleep(2)
+                pontuacao_j1 += 1
 
-            continuar = int(input("Deseja continuar? \n1- Sim\n2- Não"))
-
-        
-        else:
-            print('Opção inválida! Digite um valor válido.');
-            break;
+            continuar = 0
+            while continuar < 1 or continuar > 2:
+                continuar = int(input("Deseja continuar? \n1- Sim\n2- Não\n"))
+                if continuar < 1 or continuar > 2:
+                    print("*Opção inválida. Escolha 1 ou 2.*")
 
     if continuar == 2:
-        print('Você selecionou a opção de parar de jogar.\n Pontuação final:\n');
-        print('---------------')
-        print(f'|  {nome_j1}  |  {nome_j2}  |')
-        print(f'|   {pontuacao_j1}  |   {pontuacao_j2}  |')
-        print('---------------')
+        print('Você selecionou a opção de parar de jogar.\n')
+        sleep(2)
+        print('+' + '-' * 30 + '+')
+        print(f'| {"PLACAR FINAL":^28} |')
+        print('+' + '-' * 30 + '+')
+        print(f'| {nome_j1:^12} | {nome_j2:^12} |')
+        print('+' + '-' * 30 + '+')
+        print(f'| {pontuacao_j1:^12} | {pontuacao_j2:^12} |')
+        print('+' + '-' * 30 + '+')
+        sleep(2)
+        input('Pressione ENTER para voltar ao menu principal...')
